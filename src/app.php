4481f1760ca9e88e605f501e1267426cc71378d2
david <davidsanchezdiaz72@gmail.com>
2024-03-20 13:25:08 +0100

    Eliminar comentarios innecesarios

--- src/app.php
+++ src/app.php
@@ -4,7 +4,6 @@ if (isset($_GET['view'])) {
     // Si 'view' está presente, lo asignamos a la variable $view
     $view = $_GET['view'];
     // Construimos la ruta del archivo a incluir concatenando el nombre del archivo con $view y '.php'
-    // Asegúrate de agregar un separador de directorio ('/') entre el directorio 'src/views' y el nombre del archivo
     require 'src/views/' . $view . '.php';
 } else {
     // Si 'view' no está presente, incluye la vista de la página de inicio
